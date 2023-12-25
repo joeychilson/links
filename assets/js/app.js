6a3bf6f1e0f7d6ebef5914044381a45c2adaea84
Joey Chilson <joeychilson@outlook.com>
2023-12-24 20:29:05 -0600

    Bundled htmx with app

---
+++ assets/js/app.js
@@ -0,0 +1,1 @@
+const htmx = require('htmx.org');
--- layouts/base/head_templ.go
+++ layouts/base/head_templ.go
@@ -34,7 +34,7 @@ func Head(title string) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</title><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"><link rel=\"stylesheet\" href=\"/static/css/app.css\"><script src=\"https://unpkg.com/htmx.org@1.9.10\">")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</title><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"><link rel=\"stylesheet\" href=\"/static/css/app.css\"><script src=\"/static/js/app.js\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
