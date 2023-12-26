1992cd2c3ada8eb1310b3558ab8d9f08036fa5d2
Joey Chilson <joeychilson@outlook.com>
2023-12-26 06:33:00 -0600

    Fixed tailwind config

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ["./templates/components/**/*.templ", "./templates/layouts/**/*.templ", "./templates/pages/**/*.templ"],
+    content: ["./pages/**/*.templ"],
     theme: {
         extend: {},
     },
