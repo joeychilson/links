cef72da5ac38d579a445945ccc08850d525ac8f9
Joey Chilson <joeychilson@outlook.com>
2023-12-27 00:24:15 -0600

    Small fixes and add schema for comment voting

--- database/migrations/20231226055152_create_feed_tables.sql
+++ database/migrations/20231226055152_create_feed_tables.sql
@@ -36,19 +36,20 @@ CREATE INDEX idx_comments_link_id ON comments(link_id);
 CREATE INDEX idx_comments_user_id ON comments(user_id);
 CREATE INDEX idx_comments_parent_id ON comments(parent_id);
 
-CREATE TABLE comment_likes (
+CREATE TABLE comment_votes (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     comment_id UUID NOT NULL REFERENCES comments(id) ON DELETE CASCADE ON UPDATE CASCADE,
     user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    vote_type SMALLINT NOT NULL CHECK (vote_type IN (-1, 1)),
     created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
     UNIQUE (comment_id, user_id)
 );
 
-CREATE INDEX idx_comment_likes_comment_id ON comment_likes(comment_id);
-CREATE INDEX idx_comment_likes_user_id ON comment_likes(user_id);
+CREATE INDEX idx_comment_votes_comment_id ON comment_votes(comment_id);
+CREATE INDEX idx_comment_votes_user_id ON comment_votes(user_id);
 
 -- migrate:down
-DROP TABLE comment_likes;
+DROP TABLE comment_votes;
 DROP TABLE comments;
 DROP TABLE link_likes;
 DROP TABLE links;
